8848be645d8edae36bd0790bab9baca443a1cff2
BT_C09_10 <mobileappdev@bths.edu>
2026-04-24 09:17:07 -0400

    Initial Commit

---
+++ collab or similiar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  collab or similiar
+//
+//  Created by Student on 4/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ collab or similiar/collab_or_similiarApp.swift
@@ -0,0 +1,17 @@
+//
+//  collab_or_similiarApp.swift
+//  collab or similiar
+//
+//  Created by Student on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct collab_or_similiarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
