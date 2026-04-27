26e3c7bea921b102c4cdd09e391764f16ced31b8
BT_C09_26 <mobileappdev@bths.edu>
2026-04-27 09:29:36 -0400

    added text color

--- collab or similiar/ContentView.swift
+++ collab or similiar/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("changed text")
+                .foregroundStyle(.red)
             Image(systemName: "person.3")
                 .foregroundColor(.green)
                 .imageScale(.large)
